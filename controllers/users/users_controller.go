acf6f4ceb3cdccf2b4f64e5ed53d80bca3f1817b
smbirch <spencer.m.birch@gmail.com>
2022-06-14 04:04:33 -0700

    Setting up basic file structure and routes.

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,16 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me")
+}
+
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me")
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/smbirch/bookstore_users-api/app"
+
+func main() {
+	app.StartApplication()
+}

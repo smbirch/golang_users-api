08418dd26cc35049e270ed4122addf9694d2c479
smbirch <spencer.m.birch@gmail.com>
2022-06-17 14:58:13 -0700

    setting up /users route to create new user

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,31 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/smbirch/bookstore_users-api/domain/users"
 )
 
 func CreateUser(c *gin.Context) {
+	var user users.User
+	fmt.Println("user ID:", user)
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		fmt.Println("err:", err.Error())
+		//todo: Handle error!
+		return
+	}
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		fmt.Println("err:", err.Error())
+		//todo: handle json error
+		return
+	}
+
+	fmt.Println(string(bytes))
 	c.String(http.StatusNotImplemented, "implement me")
 }
 

a780423e0e6d264a8d2b8793c6b581be545e7ae4
smbirch <spencer.m.birch@gmail.com>
2022-06-17 15:06:44 -0700

    setting up users_service to accept incoming user creation data

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/smbirch/bookstore_users-api/domain/users"
+	"github.com/smbirch/bookstore_users-api/services"
 )
 
 func CreateUser(c *gin.Context) {
@@ -22,6 +23,12 @@ func CreateUser(c *gin.Context) {
 	if err := json.Unmarshal(bytes, &user); err != nil {
 		fmt.Println("err:", err.Error())
 		//todo: handle json error
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		//todo: handle user creation error
 		return
 	}
 
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,7 @@
-package controllers
+package services
+
+import "github.com/smbirch/bookstore_users-api/domain/users"
+
+func CreateUser(user users.User) (*users.User, error) {
+	return nil, nil
+}

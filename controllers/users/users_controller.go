bc4755a359a4a85b94b859130724cb70a090af25
smbirch <spencer.m.birch@gmail.com>
2022-06-19 02:53:00 -0700

    creating error handling for user creation

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,39 +1,27 @@
 package users
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/smbirch/bookstore_users-api/domain/users"
 	"github.com/smbirch/bookstore_users-api/services"
+	"github.com/smbirch/bookstore_users-api/utils/errors"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-	fmt.Println("user ID:", user)
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		fmt.Println("err:", err.Error())
-		//todo: Handle error!
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("Invalid JSON body")
+		c.JSON(restErr.Status, restErr)
 		return
 	}
-	if err := json.Unmarshal(bytes, &user); err != nil {
-		fmt.Println("err:", err.Error())
-		//todo: handle json error
-		return
-	}
-
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
-		//todo: handle user creation error
+		c.JSON(saveErr.Status, saveErr)
 		return
 	}
-
-	fmt.Println(string(bytes))
-	c.String(http.StatusNotImplemented, "implement me")
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,7 +1,14 @@
 package services
 
-import "github.com/smbirch/bookstore_users-api/domain/users"
+import (
+	"github.com/smbirch/bookstore_users-api/domain/users"
+	"github.com/smbirch/bookstore_users-api/utils/errors"
+)
 
-func CreateUser(user users.User) (*users.User, error) {
-	return nil, nil
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+
+	// return &user, &errors.RestErr{
+	// 	Status: http.StatusInternalServerError,
+	// }
 }

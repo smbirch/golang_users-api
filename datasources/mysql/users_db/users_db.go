54e2d231d93751dcaf435fcd9dcb185b75b72d76
smbirch <spencer.m.birch@gmail.com>
2022-07-18 08:47:02 -0700

    adding godotenv for environment help

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -4,17 +4,46 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+const (
+	mysql_users_username = "mysql_users_username"
+	mysql_users_password = "mysql_users_password"
+	mysql_users_host     = "mysql_users_host"
+	mysql_users_schema   = "mysql_users_schema"
+)
+
 var (
 	Client *sql.DB
+	//these may be used later for cloud hosting if I do not use godotenv.
+	//leaving here to remind myself what I was doing
+
+	username = os.Getenv(mysql_users_username)
+	password = os.Getenv(mysql_users_password)
+	host     = os.Getenv(mysql_users_host)
+	schema   = os.Getenv(mysql_users_schema)
 )
 
 func init() {
+	// this is for godotenv.
+	// godoterr := godotenv.Load()
+	// if godoterr != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+
+	// username := os.Getenv("mysql_users_username")
+	// password := os.Getenv("mysql_users_password")
+	// host := os.Getenv("mysql_users_host")
+	// schema := os.Getenv("mysql_users_schema")
+
 	datasourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		"root", "", "127.0.0.1:3306", "users_db")
+		username, password, host, schema)
+
 	var err error
 	Client, err := sql.Open("mysql", datasourceName)
 	if err != nil {

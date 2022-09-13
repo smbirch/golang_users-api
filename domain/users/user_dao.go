e43a694ba982072cd288e348e35cfa9773c2624b
smbirch <spencer.m.birch@gmail.com>
2022-09-12 17:43:52 -0700

    created function to find users by status

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,6 +1,8 @@
 package users
 
 import (
+	"fmt"
+
 	"github.com/smbirch/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/smbirch/bookstore_users-api/utils/date_utils"
 	"github.com/smbirch/bookstore_users-api/utils/errors"
@@ -8,10 +10,11 @@ import (
 )
 
 const (
-	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
-	queryGetUser    = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
-	queryUpdateUser = "UPDATE users SET first_name=?, last_name=?, email=? WHERE id=?;"
-	queryDeleteUser = "DELETE FROM users WHERE id=?;"
+	queryInsertUser       = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
+	queryGetUser          = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
+	queryUpdateUser       = "UPDATE users SET first_name=?, last_name=?, email=? WHERE id=?;"
+	queryDeleteUser       = "DELETE FROM users WHERE id=?;"
+	queryFindUserByStatus = "SELECT id, first_name, last_name, email, date_created, status FROM users WHERE satus=?;"
 )
 
 func (user *User) Get() *errors.RestErr {
@@ -70,12 +73,40 @@ func (user *User) Delete() *errors.RestErr {
 	if err != nil {
 		return errors.NewInternalServerError(err.Error())
 	}
-
 	defer stmt.Close()
 
 	if _, err = stmt.Exec(user.Id); err != nil {
 		return mysql_utils.ParseError(err)
 	}
-
 	return nil
+}
+
+func (user *User) FindByStatus(status string) ([]User, *errors.RestErr) {
+	stmt, err := users_db.Client.Prepare(queryFindUserByStatus)
+	if err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(status)
+	if err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
+	}
+	defer rows.Close()
+
+	results := make([]User, 0)
+	// moves pointer to next row
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+			return nil, mysql_utils.ParseError(err)
+		}
+
+		results = append(results, user)
+	}
+	if len(results) == 0 {
+		return nil, errors.NewNotFoundError(fmt.Sprintf("no users matching status %s", status))
+	}
+	return results, nil
+
 }
